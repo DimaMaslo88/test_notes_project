cc234c84b2612d7f47f184e9c149f634a4b172f7
DimaMaslo88 <dmitrij-maslo@mail.ru>
2022-12-21 16:07:54 +0300

    'feat:footer'

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Notes} from "../src/ui/components/Notes";
 import s from "../src/ui/styles/mainBlock.module.css"
 
 import {MenuItem} from "ui/components/menuItem/menuItem";
+import {Footer} from "ui/components/footer/footer";
 
 function App() {
 
@@ -11,6 +12,8 @@ function App() {
         <div className={s.mainBlock}>
             <MenuItem/>
             <Notes/>
+
+            <Footer/>
 
 
         </div>
---
+++ src/ui/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Footer = () => {
+    return (
+        <div>
+            © all rights reserved/ Dmitry Maslo 2022
+        </div>
+    );
+};
+
+
